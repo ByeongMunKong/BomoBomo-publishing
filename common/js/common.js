97ddd4c5c4b5b1280cc1ac91f734e0a0200d9383
bokkaa <bokyh0327@gmail.com>
2023-09-14 09:48:45 +0900

    파일 경로 수정 및 board폴더 추가

--- common/js/common.js
+++ common/js/common.js
@@ -3,7 +3,7 @@
 //인텔리제이에 옮기기 전 임시방편
 $(document).ready(function(){
     // $('.side-banner').load('/common/html/side-banner.html');
-    $('.header').load('./header.html');
-    $('.footer').load('./footer.html');
+    $('.header').load('/common/html/header.html');
+    $('.footer').load('/common/html/footer.html');
  
 })
